91f9292a6c1beb350cc64290f8336344e7358127
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-02-08 14:15:58 +0200

    Added a bottom navigation bar with four tabs for app sections

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,4 +15,55 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A2C5A),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF1A2C5A),
+        selectedItemColor: const Color(0xFFFFB800),
+        unselectedItemColor: Colors.white54,
+        selectedLabelStyle: const TextStyle(fontSize: 10),
+        unselectedLabelStyle: const TextStyle(fontSize: 10),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 22),
+            label: 'Summaries',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.quiz_outlined, size: 22),
+            label: 'Quizes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.announcement_outlined, size: 22),
+            label: 'Announcements',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline, size: 22),
+            label: 'Planners',
+          ),
+        ],
+      ),
+    );
+  }
+}
 
